98c379ea37f90ff8383b9e1c30f4f13614a9e70d
Marte Rodriguez <marte.rod0712@gmail.com>
2022-11-04 20:11:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import { cards } from './data.js'
 
 const root = document.getElementById('root')
 const modal = document.querySelector('.modal')
-const modalTwo = document.querySelector('.modal-two')
+const modalFooter = document.querySelector('.modal-footer')
 const fail = document.querySelector('.fail-modal')
 const dice = document.getElementById('roll-dice')
+const playerOne = document.querySelector('.player-one')
+const playerTwo = document.querySelector('.player-two')
+
+let clicks = 0;
 const cardChosen = []
 
+// buttons
 document.getElementById('start-btn').addEventListener('click', () => {
     modal.style.display = 'none'
     for(let i = cards.length - 1; i > 0; i--) {
@@ -20,15 +25,48 @@ document.getElementById('start-btn').addEventListener('click', () => {
 
 document.getElementById('reset-btn').addEventListener('click', () => {
     modal.style.display = "block"
-    modalTwo.style.display = "none"
+    modalFooter.style.display = "none"
     window.location.reload()
 })
 
+// dice roll
+function rollDice() {
+    return Math.floor(Math.random() * 3 + 1)
+}
+
 dice.addEventListener('click', () => {
-    const diceRoll = Math.floor(Math.random() * 3 + 1)
-    dice.textContent = diceRoll
+    dice.textContent = rollDice()
 })
 
+// the game
+root.addEventListener('click', () => {
+    clicks += 1
+    const diceContent = dice.textContent
+    const number = Number(diceContent)
+
+    console.log(clicks)
+    if(playerOne.classList.contains('active')) {
+        if(clicks === number) {
+            console.log('they match')
+            clicks = 0
+            playerOne.classList.remove('active')
+            playerTwo.classList.add('active')
+            dice.textContent = `Player 2 turn`
+        } 
+    } 
+    
+    if(playerTwo.classList.contains('active')) {
+        if(clicks === number) {
+            console.log('they match')
+            clicks = 0
+            playerTwo.classList.remove('active')
+            playerOne.classList.add('active')
+            dice.textContent = `Player 1 turn`
+        }
+    }
+})
+
+// board creation with flipping cards
 function createBoard() {
     for(let i = 0; i < cards.length; i++) {
         const card = document.createElement('img')
@@ -41,7 +79,7 @@ function createBoard() {
 
 function flipCard() {
     const cardId = this.getAttribute('data-id')
-    let audio = new Audio('./audio/fail.m4a')
+    let audio = new Audio('./audio/fail.m4a')  
     cardChosen.push(cards[cardId].name)
     this.setAttribute('src', cards[cardId].image)
     console.log(cardChosen)
@@ -52,7 +90,7 @@ function flipCard() {
             fail.style.display = 'none'
         }, 5000)
         setInterval(() => {
-            modalTwo.style.display = "block"
+            modalFooter.style.display = "block"
         }, 5000)   
     }
 }
